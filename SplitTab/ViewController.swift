2b877a91d41e13b51d91175428c5224b0c1ecbfe
Matthew King <mking@users.noreply.github.com>
2017-11-09 20:04:39 -0800

    Initial Commit

---
+++ SplitTab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplitTab
+//
+//  Created by matt2 on 11/9/17.
+//  Copyright © 2017 matt2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
